af7a29e5c61556085ff3770da0c673445daeca96
ruijun <june.see@dpodium.com>
2015-11-05 11:38:44 +0800

    Fix cache issue.

--- components/Filemanager.php
+++ components/Filemanager.php
@@ -43,30 +43,57 @@ class Filemanager {
         return Html::tag('div', Html::tag('i', '', ['class' => "fa {$faClass}", 'style' => "font-size: $fontSize"]), ['class' => 'fm-thumb', 'style' => "height: $height; width: $width"]);
     }
 
-    public static function getFile($keyValue, $key = 'file_id', $thumbnail = false, $tag = false) {
-        $module = \Yii::$app->getModule('filemanager');
-        $model = new $module->models['files'];
-        $fileObject = $model->find()->where([$key => $keyValue])->one();
+    public static function getFile($value, $key = 'file_id', $thumbnail = false, $tag = false) {
+        if (!in_array($key, ['file_id', 'file_identifier'])) {
+            throw new \Exception('Invalid attribute key.');
+        }
 
-        $file = [];
+        $module = \Yii::$app->getModule('filemanager');
+        $cacheKey = 'files' . '/' . $key . '/' . $value;
+
+        if (isset($module->cache)) {
+            if (is_string($module->cache) && strpos($module->cache, '\\') === false) {
+                $cache = \Yii::$app->get($module->cache, false);
+            } else {
+                $cache = Yii::createObject($module->cache);
+            }
+
+            if ($file = $cache->get($cacheKey)) {
+                return $file;
+            }
+        }
+
+        $model = new $module->models['files'];
+        $fileObject = $model->find()->where([$key => $value])->one();
+
+        $file = null;
         if ($fileObject) {
             foreach ($fileObject as $attribute => $value) {
                 $file['info'][$attribute] = $value;
             }
 
             $src = $fileObject->object_url . $fileObject->src_file_name;
-            if ($thumbnail) {
+            if ($thumbnail && !is_null($fileObject->dimension)) {
                 $src = $fileObject->object_url . $fileObject->thumbnail_name;
             }
-            $file['img'] = Html::img($src);
-        }
 
-        if ($tag && isset($fileObject->filesRelationships)) {
-            foreach ($fileObject->filesRelationships as $relationship) {
-                if (isset($relationship->tag)) {
-                    $file['tag'][$relationship->tag->tag_id] = $relationship->tag->value;
+            if (!is_null($fileObject->dimension)) {
+                $file['img'] = Html::img($src);
+            }
+
+            if ($tag && isset($fileObject->filesRelationships)) {
+                foreach ($fileObject->filesRelationships as $relationship) {
+                    if (isset($relationship->tag)) {
+                        $file['tag'][$relationship->tag->tag_id] = $relationship->tag->value;
+                    }
                 }
             }
+        }
+
+        if ($file !== null && isset($cache)) {
+            $cache->set($cacheKey, $file, 86400, new \yii\caching\TagDependency([
+                'tags' => self::CACHE_TAG
+            ]));
         }
 
         return $file;
